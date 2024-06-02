2672ba00c5060f160e4ecfecdf34309a42675076
enmebaraggesi <koniec.poczatek@gmail.com>
2024-06-02 22:34:27 +0200

    Initial commit

---
+++ src/main/java/com/songify_rest_template_client/SongifyRestTemplateClientApplication.java
@@ -0,0 +1,13 @@
+package com.songify_rest_template_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyRestTemplateClientApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyRestTemplateClientApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/songify_rest_template_client/SongifyRestTemplateClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songify_rest_template_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongifyRestTemplateClientApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
